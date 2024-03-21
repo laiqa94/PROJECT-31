e44a2350673fc8e9b8c2b8d30df1f7ffbf8b858c
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:38:49 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames = ['admin', 'ameen ', 'hamza ', 'zia', 'kamran'];
+if (userNames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    userNames = [];
+    console.log('All user Names have been removed.' + userNames.length);
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+
+let userNames:string[] =['admin','ameen ','hamza ','zia','kamran'];
+
+if(userNames.length === 0){
+    console.log('We need to find some users!');
+}else{
+    userNames =[];
+    console.log('All user Names have been removed.' + userNames.length);
+}
